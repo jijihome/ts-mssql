0ff3dce9316e510a9b3e9850c6d991c9890789c6
jijihome <jijihome@gmail.com>
2017-09-13 10:53:44 +0800

    TypeScript环境配置完成

---
+++ dist/app.js
@@ -0,0 +1,16 @@
+/**
+ * person
+ */
+class person {
+    constructor() {
+    }
+    /**
+     * print
+     */
+    print() {
+        console.log('this is a person 123');
+    }
+}
+let p = new person();
+p.print();
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,19 @@
+/**
+ * person
+ */
+class person {
+  constructor() {
+
+  }
+
+  /**
+   * print
+   */
+  public print() {
+    console.log('this is a person 123')
+  }
+}
+
+
+let p = new person();
+p.print();
